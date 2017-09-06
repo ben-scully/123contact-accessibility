ada39b7b0e60d44ecd3ffaf2cd180a28469acaa0
ben-scully <ben@moregolf.co.nz>
2017-09-06 22:05:25 +1200

    added console.log before and after

--- update-electorate.js
+++ update-electorate.js
@@ -1,9 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("message", function(event) {
+    console.log("event BEFORE", event);
     if (event.orgin.contains("https://accessalliance.nationbuilder.com") == -1)
       return;
-    console.log("event", event);
+    console.log("event AFTER", event);
 
     var data = event.data;
     console.log("data", data);
