b97063e672162af00b0b779016a131f0711d8d70
ben-scully <ben@moregolf.co.nz>
2017-09-06 22:12:03 +1200

    changed contains to indexOf

--- update-electorate.js
+++ update-electorate.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("message", function(event) {
     console.log("event BEFORE", event);
-    if (event.orgin.contains("https://accessalliance.nationbuilder.com") == -1)
+    if (event.orgin.indexOf("https://accessalliance.nationbuilder.com") == -1)
       return;
     console.log("event AFTER", event);
 
@@ -12,6 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var electorate = data.electorate;
     console.log("electorate", electorate);
 
-    $("#id123-control31500616").val(electorate).change()
+    $("#id123-control31500616").val(electorate).change();
   });
 });
