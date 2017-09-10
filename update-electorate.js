363b6139ec29e3446acf69f01593c112840179b2
ben-scully <ben@moregolf.co.nz>
2017-09-10 21:09:11 +1200

    update

--- update-electorate.js
+++ update-electorate.js
@@ -6,10 +6,46 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var electorate = getParameterByName("electorate", url);
   console.log("electorate", electorate);
-  $("#id123-control31500616").val(electorate).change();
+  $("#id123-control32357384").val(electorate).change();
 
-  var letter = getParameterByName("letter", url);
-  console.log("letter", letter);
+
+
+  var insertContent = "\n";
+
+  var kids = getParameterByName("kids", url);
+  console.log("kids", kids);
+  if (kids)
+    insertContent += "People in families with children aged 0-4: " + kids + "\n";
+
+  var old = getParameterByName("old", url);
+  console.log("old", old);
+  if (old)
+    insertContent += "Over 75yrs: " + old + "\n";
+
+  var live = getParameterByName("live", url);
+  console.log("live", live);
+  if (live)
+    insertContent += "Supported Living Beneficiaries: " + live + "\n";
+
+  var sick = getParameterByName("sick", url);
+  console.log("sick", sick);
+  if (sick)
+    insertContent += "Sickness Beneficiaries: " + sick + "\n";
+
+  var total = getParameterByName("total", url);
+  console.log("total", total);
+  if (total)
+    insertContent += "Total people with access sensitivity: " + total + "\n";
+
+  var emailBody = $("#id123-control32357387");
+  var prevEmailBody = emailBody.val();
+  console.log("prevEmailBody", prevEmailBody);
+
+  var updatedEmailBody = prevEmailBody.replace("@@@@", insertContent);
+  console.log("updatedEmailBody", updatedEmailBody);
+  emailBody.val(updatedEmailBody);
+
+
 
   function getParameterByName(name, url) {
     if (!url) url = window.location.href;
